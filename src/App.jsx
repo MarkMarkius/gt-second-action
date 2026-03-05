ed0c379c2917f3b671e195f7bd3d08572d03eccd
MarkMarkius <markmarkius@gmail.com>
2026-03-05 08:31:40 +0200

    Dev (#1)
    * add content
    
    * add text 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h1>Learn & Master GitHub Actions</h1>
+        <h1>Learn & Master GitHub Actions</h1>
       </header>
       <MainContent />
     </>
